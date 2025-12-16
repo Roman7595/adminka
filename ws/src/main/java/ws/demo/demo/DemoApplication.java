8ed745bc1ce5127452f4499d8839275f4f370681
Roman <chernyak.roman@mail.com>
2025-12-16 03:26:37 +0300

    fix 25

--- ws/src/main/java/ws/demo/demo/DemoApplication.java
+++ ws/src/main/java/ws/demo/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package ws.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.actuate.autoconfigure.web.mappings.ServletMappingsAutoConfiguration.class
+})
 public class DemoApplication {
 
 	public static void main(String[] args) {
