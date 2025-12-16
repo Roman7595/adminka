77363af4e022b125727d35b156b5f5f0938d5cd1
Roman <chernyak.roman@mail.com>
2025-12-16 03:36:57 +0300

    try 27

--- ws/src/main/java/ws/demo/demo/DemoApplication.java
+++ ws/src/main/java/ws/demo/demo/DemoApplication.java
@@ -3,9 +3,7 @@ package ws.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.actuate.autoconfigure.web.mappings.ServletMappingsAutoConfiguration.class
-})
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
